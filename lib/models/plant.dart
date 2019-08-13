1a359c6f3be94dde0492039b63d2b9694fa92b0d
Benjamin Kostiuk <benjamin.kostiuk@peelregion.ca>
2019-08-13 14:48:28 -0400

    added hero tag property

--- lib/models/plant.dart
+++ lib/models/plant.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Plant {
   String name;
   String location;
@@ -5,9 +7,12 @@ class Plant {
   String assetName;
   double currentPercentage;
   bool isDry;
+  String heroTag;
 
   Plant(this.name, this.location, this.waterConsumption, this.assetName,
       this.currentPercentage) {
+    var rng = Random();
+    heroTag = name + location + assetName + rng.nextInt(100).toString();
     isDry = (currentPercentage > 0) ? false : true;
   }
 }
