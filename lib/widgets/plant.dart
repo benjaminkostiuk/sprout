b3ac08bde83e0f1eab337f84a13db9e0979c905d
Kostiuk <benjamin.kostiuk@peelregion.ca>
2019-08-13 11:50:55 -0400

    added droplet indicator animation and generalized card body

--- lib/widgets/plant.dart
+++ lib/widgets/plant.dart
@@ -24,11 +24,8 @@ class PlantCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.only(right: 0.0, left: 0.0, top: 5.0, bottom: 5.0),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(18.0)),
-      ),
+    return PlantCardBody(
+      color: Colors.white,
       child: Row(
         children: <Widget>[
           Container(
@@ -109,22 +106,85 @@ class PlantCard extends StatelessWidget {
               ],
             ),
           ),
-          Container(
-            margin: EdgeInsets.only(right: 25.0),
-            child: CustomPaint(
-              foregroundPainter: DropletPainter(plant.currentPercentage),
-              child: Padding(
-                padding: const EdgeInsets.all(13.5),
-                child: Icon(
-                  CustomIcons.droplet,
-                  size: 25.0,
-                  color:
-                      plant._isDry ? Colors.grey[500] : Colors.lightGreen[400],
-                ),
-              ),
-            ),
-          ),
+          DropletIndicator(plant.currentPercentage, plant._isDry),
         ],
+      ),
+    );
+  }
+}
+
+class PlantCardBody extends StatelessWidget {
+  final Widget child;
+  final Color color;
+  PlantCardBody({@required this.color, this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.only(right: 0.0, left: 0.0, top: 5.0, bottom: 5.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(18.0)),
+      ),
+      child: child ?? Container(),
+      color: color,
+    );
+  }
+}
+
+class DropletIndicator extends StatefulWidget {
+  final double percentage;
+  final bool isDry;
+
+  DropletIndicator(this.percentage, this.isDry);
+
+  @override
+  State<StatefulWidget> createState() => _DropletIndicatorState();
+}
+
+class _DropletIndicatorState extends State<DropletIndicator>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation<double> _animatePercentage;
+
+  @override
+  void initState() {
+    // Initialize animation controller
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 1400))
+          ..addListener(() {
+            setState(() {});
+          });
+
+    // Setup the animation for the _filledPercentage to increase
+    _animatePercentage =
+        Tween<double>(begin: 0.0, end: widget.percentage).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.ease),
+    );
+    // Make the animation run
+    _animationController.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 25.0),
+      child: CustomPaint(
+        foregroundPainter: DropletPainter(_animatePercentage.value),
+        child: Padding(
+          padding: const EdgeInsets.all(13.5),
+          child: Icon(
+            CustomIcons.droplet,
+            size: 25.0,
+            color: widget.isDry ? Colors.grey[500] : Colors.lightGreen[400],
+          ),
+        ),
       ),
     );
   }
@@ -161,6 +221,6 @@ class DropletPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
+    return true;
   }
 }
