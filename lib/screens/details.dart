87428c8d4a210bde0d096c0d87816f9ea5fbf969
Benjamin Kostiuk <benjamin.kostiuk@peelregion.ca>
2019-08-13 16:24:17 -0400

    added sample hero code

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -10,15 +10,16 @@ class PlantDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: RaisedButton(
-        onPressed: () {
-          // Navigate back to the first screen by popping the current route
-          // off the stack.
-          Navigator.pushNamed(context, '/add');
-        },
-        child: Text('Plant Details'),
+        body: Container(
+      child: Column(
+        children: <Widget>[
+          Text(plant.name),
+          Hero(
+            tag: plant.heroTag,
+            child: Image.asset(plant.assetName),
+          ),
+        ],
       ),
-    ),);
+    ));
   }
 }
