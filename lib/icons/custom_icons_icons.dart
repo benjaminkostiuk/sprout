bffd64c629d6a87617ec0f6658dbaf9876a699de
Benjamin Kostiuk <benjamin.kostiuk@peelregion.ca>
2019-08-20 13:48:28 -0400

    updated custom icons for use on details page

--- lib/icons/custom_icons_icons.dart
+++ lib/icons/custom_icons_icons.dart
@@ -16,6 +16,14 @@
 ///         Author:    Daniel Bruce
 ///         License:   SIL (http://scripts.sil.org/OFL)
 ///         Homepage:  http://www.entypo.com
+/// * Typicons, (c) Stephen Hutchings 2012
+///         Author:    Stephen Hutchings
+///         License:   SIL (http://scripts.sil.org/OFL)
+///         Homepage:  http://typicons.com/
+/// * Material Design Icons, Copyright (C) Google, Inc
+///         Author:    Google
+///         License:   Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
+///         Homepage:  https://design.google.com/icons/
 /// * Font Awesome, Copyright (C) 2016 by Dave Gandy
 ///         Author:    Dave Gandy
 ///         License:   SIL ()
@@ -32,5 +40,21 @@ class CustomIcons {
   static const IconData droplet = const IconData(0xe801, fontFamily: _kFontFam);
   static const IconData plant = const IconData(0xe802, fontFamily: _kFontFam);
   static const IconData watering_can_with_water_drops = const IconData(0xe803, fontFamily: _kFontFam);
+  static const IconData light_down = const IconData(0xe804, fontFamily: _kFontFam);
+  static const IconData light_up = const IconData(0xe805, fontFamily: _kFontFam);
+  static const IconData temperatire = const IconData(0xe806, fontFamily: _kFontFam);
+  static const IconData water = const IconData(0xe807, fontFamily: _kFontFam);
+  static const IconData search = const IconData(0xe808, fontFamily: _kFontFam);
+  static const IconData data_usage = const IconData(0xe809, fontFamily: _kFontFam);
+  static const IconData settings = const IconData(0xe80a, fontFamily: _kFontFam);
+  static const IconData chart_line = const IconData(0xe80b, fontFamily: _kFontFam);
+  static const IconData chart_bar = const IconData(0xe80c, fontFamily: _kFontFam);
+  static const IconData timeline = const IconData(0xe80d, fontFamily: _kFontFam);
+  static const IconData person_outline = const IconData(0xe80e, fontFamily: _kFontFam);
+  static const IconData lightbulb_outline = const IconData(0xe80f, fontFamily: _kFontFam);
+  static const IconData wb_sunny = const IconData(0xe810, fontFamily: _kFontFam);
+  static const IconData sun = const IconData(0xe811, fontFamily: _kFontFam);
+  static const IconData sun_filled = const IconData(0xe812, fontFamily: _kFontFam);
   static const IconData location = const IconData(0xf124, fontFamily: _kFontFam);
+  static const IconData connectdevelop = const IconData(0xf20e, fontFamily: _kFontFam);
 }
